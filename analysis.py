2469766aa212ef2ea95ac9512d2e8a9a19d8abf3
michael1241 <michael@jamcupboard.co.uk>
2021-11-29 21:30:53 +0000

    working version for 1 study

--- analysis.py
+++ analysis.py
@@ -5,33 +5,32 @@ import chess.pgn
 import itertools
 from statistics import mean
 
-f = open('analysed_pgns/lichess_study_1894-chess-world-championship-steinitz-lasker_by_Lichess_2021.11.29.pgn')
 
+def pgnProcess(pgn, event):
 
-output = {}
-
-
-def pgnProcess(pgn):
-
-   def pairwise(iterable):
+    def pairwise(iterable):
         # pairwise('ABCDEFG') --> AB BC CD DE EF FG
         a, b = itertools.tee(iterable)
         next(b, None)
         return zip(a, b)
 
+    output = {event: {}}
     count = 0
     while True:
-        count += 1
-        print(count)
+
         game = chess.pgn.read_game(f)
         if not game:
             break
+
+        count += 1
+        output[event][str(count)] = dict()
+
         w = game.headers['White']
         b = game.headers['Black']
 
         evals = []
         for node in game.mainline():
-            evals.append(max(min(node.eval().white(), chess.engine.Cp(1000)), chess.engine.Cp(-1000))  # blunders above 1000 don't count
+            evals.append(max(min(node.eval().white(), chess.engine.Cp(1000)), chess.engine.Cp(-1000)).score())  # blunders above 1000 don't count
 
         def difference(n, evals):
             a, b = evals
@@ -45,5 +44,13 @@ def pgnProcess(pgn):
 
         black_evals = cpls[::2]
         white_evals = cpls[1::2]
-        print(mean(white_evals))
-        print(mean(black_evals))
+        output[event][str(count)][w] = mean(white_evals)
+        output[event][str(count)][b] = mean(black_evals)
+    return output
+
+
+event = 'lichess_study_1894-chess-world-championship-steinitz-lasker_by_Lichess_2021.11.29.pgn'
+f = open(f'analysed_pgns/{event}')
+
+output = pgnProcess(f, event)
+print(output)

7991207abccced681e93b44dd558c2a0361ad2c7
michael1241 <michael@jamcupboard.co.uk>
2021-11-30 13:39:59 +0000

    color by champ

--- visualize.py
+++ visualize.py
@@ -40,7 +40,7 @@ df = pd.read_csv('analysis.csv')
 # Cols = Year, Game #, White, Black, Comb. ACPl
 cols_to_get = [1, 2, 3, 6, 9]
 pretty_df = df[df.columns[cols_to_get]]
-print(pretty_df.sort_values('Combined ACPL', ascending=True))
+#print(pretty_df.sort_values('Combined ACPL', ascending=True))
 
 # Output 2: Match ACPL violin plots vs. time
 
@@ -76,7 +76,7 @@ sns.boxenplot(x='Year', y='Combined ACPL', data=df, ax = axes)
 axes.set_title('Combined ACPL by WC Year')
 axes.set_xticklabels(axes.get_xticklabels(), rotation=60)
 plt.savefig('boxenplot.png')
-plt.cla()"""
+plt.cla()
 
 color_df = unweighted_combined_acpls(df)
 highest_acpl = color_df['acpl'].max()
@@ -84,14 +84,23 @@ highest_acpl = color_df['acpl'].max()
 color_df['color'] = color_df.apply(lambda row: strRgb(row['acpl'], 0, highest_acpl), axis=1)
 
 palette = list(color_df.color)
+"""
 
-sns.boxplot(x='Year', y='Combined ACPL', data=df, ax=axes, palette=palette)
+champs = pd.read_csv('champs.csv')
+df = pd.merge(df, champs, how='left')
+pd.options.display.width = 0
+
+palette = {WC: color for (WC, color) in zip(df.WC.unique(), sns.color_palette() + sns.color_palette("pastel"))}
+print(df)
+
+sns.boxplot(x='Year', y='Combined ACPL', data=df, ax=axes, hue="WC", palette=palette, dodge=False)
 axes.set_title('Combined ACPL by WC Year')
 axes.set_xticklabels(axes.get_xticklabels(), rotation=60)
-plt.savefig('boxplot_colored_acpl.png')
+plt.savefig('boxplot_color_champ.png')
+plt.show()
 plt.cla()
-
-"""sns.violinplot(x='Year', y='Combined ACPL', data=df, ax = axes, linewidth = 0.01)
+"""
+sns.violinplot(x='Year', y='Combined ACPL', data=df, ax = axes, linewidth = 0.01)
 axes.set_title('Combined ACPL by WC Year')
 axes.set_xticklabels(axes.get_xticklabels(), rotation=60)
 plt.savefig('violinplot.png')
@@ -132,6 +141,7 @@ plt.cla()
 fig, axes = plt.subplots(figsize=(12, 9))
 sns.scatterplot(x='White Num Moves', y='Combined ACPL', hue='Year', data=df)
 axes.set_ylim((-5, 105))
-plt.savefig('gamelengthscatterplot.png')"""
+plt.savefig('gamelengthscatterplot.png')
 
 # In retrospect this plot is hard to read and not conclusive.
+"""
